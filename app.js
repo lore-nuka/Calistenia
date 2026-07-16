dcde3e8aca4e8c45736d099da85f77103cb0c61d
lore-nuka <lorenagargar@gmail.com>
2026-07-17 00:15:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -50,3 +50,98 @@ window.onload = function(){
     actualizarCabecera();
 
 }
+const entrenamientos = {
+
+principiante: [
+
+[
+"10 Flexiones",
+"20 Sentadillas",
+"15 Abdominales",
+"30 segundos Plancha"
+],
+
+[
+"12 Flexiones inclinadas",
+"25 Sentadillas",
+"20 Mountain Climbers",
+"40 segundos Plancha"
+],
+
+[
+"8 Burpees",
+"15 Fondos en banco",
+"20 Zancadas",
+"30 Jumping Jacks"
+]
+
+],
+
+intermedio: [
+
+[
+"20 Flexiones",
+"40 Sentadillas",
+"10 Dominadas",
+"1 minuto Plancha"
+],
+
+[
+"15 Fondos",
+"30 Zancadas",
+"20 Burpees",
+"15 Elevaciones de piernas"
+]
+
+],
+
+avanzado: [
+
+[
+"40 Flexiones",
+"20 Dominadas",
+"60 Sentadillas",
+"2 minutos Plancha"
+],
+
+[
+"25 Burpees",
+"30 Fondos",
+"20 Dominadas explosivas",
+"100 Saltos"
+]
+
+]
+
+};
+function obtenerNivel(){
+
+if(estado.nivel<=1)
+return "principiante";
+
+if(estado.nivel==2)
+return "intermedio";
+
+return "avanzado";
+
+}
+function generarEntrenamiento(){
+
+const nivel = obtenerNivel();
+
+const lista = entrenamientos[nivel];
+
+const indice =
+Math.floor(Math.random()*lista.length);
+
+return lista[indice];
+
+}
+function mostrarEntrenamiento(){
+
+const entrenamiento =
+generarEntrenamiento();
+
+alert(entrenamiento.join("\n"));
+
+}
